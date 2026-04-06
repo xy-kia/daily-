4520ba110eb582bb2d3bf796aef634ec90205cd9
xy-kia <2993405609@qq.com>
2026-04-06 13:14:16 +0800

    25.4.6

---
+++ Cpp/26.4/26.4.6/code.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        long long x=0;
+        long long y=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='U'){
+                y++;
+            }else if(moves[i]=='D'){
+                y--;
+            }else if(moves[i]=='L'){
+                x--;
+            }else{
+                x++;
+            }
+        }
+        return (x==0) && (y==0);
+    }
+};
