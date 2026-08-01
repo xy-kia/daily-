61229cfb9cf29e7da922cb68c395a1c2af1db8fe
xy-kia <2993405609@qq.com>
2026-08-01 22:57:56 +0800

    26.8.1

---
+++ 26.8/26.8.1/code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        if(n <= 8){
+            return n;
+        }else if(n <= 16){
+            return (8 + 2 * (n - 8));
+        }else if(n <= 24){
+            return 24 + 3 * (n - 16);
+        }else{
+            return 48 + 4 * (n - 24);
+        }
+
+    }
+};
